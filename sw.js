5e63f4d6fa41b69ffd80b81875af0c72ef37259c
VictorVargasC <luciavargascortes@gmail.com>
2026-09-17 20:08:03 -0500

    Game Timer: temporizador para juegos de mesa

---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = "game-timer-v1";
+var SHELL = ["./index.html", "./manifest.json", "./icon.svg", "./icon-maskable.svg"];
+
+self.addEventListener("install", function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(SHELL).catch(function(){ /* ignore individual failures */ });
+    })
+  );
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener("fetch", function(event){
+  if(event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      var network = fetch(event.request).then(function(resp){
+        if(resp && resp.ok && event.request.url.indexOf(self.location.origin) === 0){
+          var copy = resp.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+        }
+        return resp;
+      }).catch(function(){ return cached; });
+      return cached || network;
+    })
+  );
+});
